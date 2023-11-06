3b4b5e3d6ae297fc5110867ade8903f37fb1917d
Aesley - HP <aesley.soares@irede.org.br>
2023-11-06 12:08:48 -0300

    alterções no codigo

--- src/main/java/org/json/JSONGeneratortxt.java
+++ src/main/java/org/json/JSONGeneratortxt.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
-
+//FUNCIONAL
 public class JSONGeneratortxt {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
